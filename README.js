9803fdb6b39cbf5e1d5e8df2e4738c0c65f2660e
Oriol <110051760+oriolgds@users.noreply.github.com>
2022-09-09 16:18:08 +0200

    Update README.js

--- README.js
+++ README.js
@@ -6,3 +6,9 @@ if (sad() === true){
 while (living() === true) {
   code().allDay();
 }
+document.querySelector("body").innerHTML = `
+- Student at Institut de Sales
+- Making code an universal language
+- Love the people that make the same as me
+- Always building projects to learn
+`;
